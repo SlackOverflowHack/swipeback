9e8998fb8b5132a44aafac2776d44495ff2b966c
Jonas <mail@jonasgoebel.net>
2022-10-29 23:11:47 +0200

    added firebase authentication

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SwipeBack
 ## The backend for swiper
+
+## Connect from php to Firebase FireStore
+https://developers.google.com/identity/protocols/oauth2/service-account#creatinganaccount
--- routes/api.php
+++ routes/api.php
@@ -20,47 +20,5 @@ Route::prefix('user')->middleware('auth:api', 'throttle:clientApp')->group(funct
         Route::get('/', function (Request $request) {
                 return $request->user();
         });
-
-	Route::post('createHomeToken', 'App\Http\Controllers\ApiController@createHomeToken');
 });
 
-
-/**
- * routes for lamaframe integration (only from frame)
- */
-Route::prefix('frame')->middleware('auth:frameDevice', 'throttle:frameDevice')->group(function(){
-	Route::get('image/{imageID}', 'App\Http\Controllers\ImagesController@getImage');
-	Route::get('imageIDs', 'App\Http\Controllers\FramesController@getImageIDs');
-});
-
-/**
- * routes from alexa / bixby
- * oauth will be used for authentication
- * client user (not alexa etc) is authenticated
- */
-Route::prefix('integrations')->middleware(['auth:api', 'scope:devices-read'])->group(function() {
-	Route::prefix('alexa')->group(function() {
-		Route::get('discover', 'App\Http\Controllers\Integrations\AlexaController@handleDiscoveryRequest');
-		Route::post('stateRefresh', 'App\Http\Controllers\Integrations\AlexaController@handleStateRefreshRequest');
-		Route::middleware('scope:devices-set')->post('setState', 'App\Http\Controllers\Integrations\AlexaController@handleSetStateRequest');
-	});
-
-	Route::post('smartthings', 'App\Http\Controllers\SmartThingsController@receiveMessage');
-});
-
-/// route for moving the token from a SmartThings response into the header. message will be resent
-Route::post('integrations/smartthingsTokenInRequestParameter', 'App\Http\Controllers\SmartThingsController@resendRequestWithHeader');
-
-Route::prefix('homes')->middleware('throttle:localInstances')->group(function(){
-	Route::middleware('auth:api')->get('/', 'App\Http\Controllers\HomesController@get');
-	Route::post('/', 'App\Http\Controllers\HomesController@create');
-	Route::middleware('auth:token')->group(function() {
-	    Route::post('connectionInfo', 'App\Http\Controllers\HomesController@getConnectionInfo');
-	    Route::post('unlink', 'App\Http\Controllers\HomesController@unlinkLocalInstance');
-	});
-});
-
-Route::prefix('mosquitto')->middleware('throttle:mqttAuth')->group(function() {
-    Route::post('auth', 'App\Http\Controllers\MosquittoController@authenticateHome');
-    Route::post('acl', 'App\Http\Controllers\MosquittoController@checkTopicPermission');
-});
